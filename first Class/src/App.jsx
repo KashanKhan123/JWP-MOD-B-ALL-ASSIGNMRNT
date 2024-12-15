7f7784f506e90c536b6d548c2c36a928c857f8f4
Kashan Khan <kashankhan123hafiz@gmail.com>
2024-12-15 17:59:28 +0500

    Add files via upload

---
+++ first Class/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>my name is kashan</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit deleniti fugit quisquam consequatur dolore tenetur non dicta consequuntur iusto eveniet, voluptatem sapiente quam amet maiores molestias error modi aspernatur at?</p>
+      </div>
+    </>
+  )
+}
+
+export default App
